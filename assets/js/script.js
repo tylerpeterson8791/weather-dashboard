93b2ae25d79a45a70d67198d06cba3e99a457485
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-01-10 00:24:47 -0600

    third-draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,20 @@
+//Event Listner for searchbtn
+//  fetch from lat/long api
+//  make into a variable (lets say latLon)
+//      use latlon to do a fetch from the 5Day api
+//          convert pertinent data into variables
+//              push those variables to populate in html into appropriate fields
+//                  3HR BLOCK ON 5DAY - Need to figure out how to target appropriate dayblocks for 5Day Display
+//
+//Take latlon and store it so it can be called again for the history buttons
+//  commit to local storage
+//  figure out how to display the city name on the buttons themselves
+//      program button listeners for clicks on those   
+//          upon click run the fetch for the 5Day api using stored latlon
+//          push variables into appropriate html
+//              HOW DO I HANDLE ADDING NEW CITIES AND BUMPING THE LIST. CONTINUOUSLY ADD AND ONLY DISPLAY FIRST 8?  I SMELL AN ARRAY
+//
+//HOW DID THEY DO THE EMOJI THING??
 
-//Check api and see if date is necessary, if so use day.js to define current date.  Probably need this anyway for infobox display 
-//Listener on search button as well as the previous search buttons
-//
-//Think about the buble up and how with the previous searches the variable will be autofilled as opposed to grabbing the textbox on a new one.
-//
-//^^^^Actually I want to set it up so the gray buttons automatically go when clicked so no need for the second click of the search?  Is this possible?  Easier to just reping the API?
-//
-// Previous search buttons should only appear if there's previous searches.  The example shows 8 slots, that should be sufficient.  Once the user reaches 8 previous searches, how do we handle "bumping" the newest one in for the oldest???  Replace them in an array has to be how to approach it.  Think on that but start build as static.
-//
-//Everything else should just be point and go from the API as far as the info we pop into divs
-//
-//How does it handle a misspelled city name? Once it's done try to break it. 
-//
-//If there's an error do we pop an alert?  Send them to a different page explaining error?
-//
-// USE TWO DIFFERENT APIs, one for Lat and Lon and the other for the report
 
-// HTML almost complete, once I get IDs in there I can take a crack at this.
+
