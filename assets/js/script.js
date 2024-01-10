7bc00fcfb772bdb22397f5ae4ac9d36fea8a642e
Tyler Peterson <tylerpeterson8791@gmail.com>
2024-01-09 22:40:15 -0600

    second-draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,19 @@
-//Tons of dynamic stuff on this one. 
+
 //Check api and see if date is necessary, if so use day.js to define current date.  Probably need this anyway for infobox display 
 //Listener on search button as well as the previous search buttons
 //
 //Think about the buble up and how with the previous searches the variable will be autofilled as opposed to grabbing the textbox on a new one.
 //
-//^^^^Actually I want to set it up so the gray buttons automatically go when clicked so no need for the second click of the search.
+//^^^^Actually I want to set it up so the gray buttons automatically go when clicked so no need for the second click of the search?  Is this possible?  Easier to just reping the API?
 //
-// Previous search buttons should only appear if there's previous searches.  The example shows 8 slots, that should be sufficient.  Once the user reaches 8 previous searches, how do we handle "bumping" the newest one in for the oldest???  Think on that but start build as static
+// Previous search buttons should only appear if there's previous searches.  The example shows 8 slots, that should be sufficient.  Once the user reaches 8 previous searches, how do we handle "bumping" the newest one in for the oldest???  Replace them in an array has to be how to approach it.  Think on that but start build as static.
 //
 //Everything else should just be point and go from the API as far as the info we pop into divs
 //
-//Does the API need to be provided with the current date?
-//
-//How does it handle a misspelled city name?  
+//How does it handle a misspelled city name? Once it's done try to break it. 
 //
 //If there's an error do we pop an alert?  Send them to a different page explaining error?
 //
-//I'm gonna check out the API and play with this and think about how I excecute.  Then come up with a skeleton tonight after the kid's bedtime.
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+// USE TWO DIFFERENT APIs, one for Lat and Lon and the other for the report
+
+// HTML almost complete, once I get IDs in there I can take a crack at this.
